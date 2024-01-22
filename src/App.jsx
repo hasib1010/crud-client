baf7a0183326b4f1c58a4ea0b3cd84cb0bf4d63c
hasib1010 <asifmahmud664@gmail.com>
2024-01-22 18:42:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+
+function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target
+    const email = form.email.value;
+    const name = form.name.value;
+    const user = { name, email }
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+  }
+  return (
+    <>
+      <div>
+        <h1>Crud Client</h1>
+
+        <form onSubmit={handleSubmit}>
+          <input className='input' placeholder='email' type="email" name='email' /><br />
+          <input className='input' placeholder='name' type="text" name='name' /> <br />
+          <input className='input' type="submit" value="Add User" />
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
