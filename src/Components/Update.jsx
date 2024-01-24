67f559f89f0753630996516944d392dd96aad9f3
hasib1010 <asifmahmud664@gmail.com>
2024-01-24 23:39:20 +0600

    Crud added

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -3,13 +3,22 @@ import { useLoaderData } from 'react-router-dom';
 
 const Update = () => {
     const loadedUser = useLoaderData();
-    const handleUpdate =(e)=>{
+    const handleUpdate = (e) => {
         e.preventDefault()
         const form = e.target;
         const email = form.email.value;
         const name = form.name.value;
-        const user = {email, name};
-        console.log(user);
+        const user = { email, name };
+        // console.log(user);
+        fetch(`http://localhost:5000/users/${loadedUser._id}`,{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(user)
+        })
+        .then(res=> res.json())
+        .then(data=> console.log(data))
     }
     return (
         <div>
